ef053c14efd87f6cf185164a22995ed95a1648bd
Kiewan Villatel <kiewan.villatel@gmail.com>
2020-02-20 19:04:47 +0100

    Add input files

--- hashcode/main.py
+++ hashcode/main.py
@@ -33,7 +33,7 @@ def compute_solutions(input_path: str, output_path: str):
     mkdir(sol_folder)
 
     # Run test cases
-    test_cases = [f for f in listdir(input_path) if isfile(join(input_path, f)) and f.endswith(".in")]
+    test_cases = [f for f in listdir(input_path) if isfile(join(input_path, f)) and f.endswith(".txt")]
     for test_case in test_cases:
         test_case_name = test_case.split(".")[0]
         print("Processing test case {}".format(test_case))
