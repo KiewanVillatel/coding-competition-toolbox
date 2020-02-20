9b0f41f8ab9939a6cfecc29538ec7dce1208e244
flagos <flagospub@gmail.com>
2020-02-20 20:40:19 +0100

    library naive score

--- hashcode/model/library.py
+++ hashcode/model/library.py
@@ -8,3 +8,16 @@ class Library:
         self.books = set(books)
         self.sign_time = sign_time
         self.books_per_day = books_per_day
+        self.compute_score()
+
+    def get_score(self):
+        return self.score
+
+    def update(self, books_scanned: List[Book]):
+        for bc in books_scanned:
+            if bc in self.books:
+                self.books.remove(bc)
+        self.compute_score()
+
+    def compute_score(self):
+        self.score = self.books_per_day * sum([b.score for b in self.books]) / self.sign_time
