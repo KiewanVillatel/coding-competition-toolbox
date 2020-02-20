2d74b6e468c8f8f5b706ecb949a1957cd81f8515
flagos <flagospub@gmail.com>
2020-02-20 22:00:27 +0100

    remove boks from library

--- hashcode/model/greedy_solution.py
+++ hashcode/model/greedy_solution.py
@@ -10,32 +10,52 @@ class GreedySolution(Solution):
     def __init__(self, problem: Problem):
         super().__init__(problem)
 
+
+    def get_next_library(self):
+        returned_lib = None
+        max_lib_score = 0
+        for lib in self.libraries_to_sign:
+            score = lib.get_score()
+            if score >= max_lib_score:
+                max_lib_score = score
+                returned_lib = lib
+        return returned_lib
+
+
+    def remove_book_in_librairies(self, book: Book):
+        for lib in self.signed_libraries:
+            lib.remove_book(book)
+
+
+
     @staticmethod
     def generate_solution(problem: Problem) -> Solution:
         sol = GreedySolution(problem)
 
         book_scanned = [False for _ in range(len(problem.books))]
 
-        libraries_to_sign = [l for l in problem.libraries]
-        signed_libraries = []
-        current_signed_library = problem.libraries[0]
-        libraries_to_sign.remove(current_signed_library)
+        sol.libraries_to_sign = [l for l in problem.libraries]
+        sol.signed_libraries = []
+        current_signed_library = sol.get_next_library()
+        sol.libraries_to_sign.remove(current_signed_library)
         remaining_time_to_sign = current_signed_library.sign_time
 
         for timestep in range(problem.deadline):
-            print("Progress {}".format(float(timestep/problem.deadline)))
+            if timestep % ((max(problem.deadline//1000, 1)) * 10) == 0:
+                print("Progress {}".format(float(timestep/problem.deadline)))
             if remaining_time_to_sign == 0:
-                signed_libraries.append(current_signed_library)
+                sol.signed_libraries.append(current_signed_library)
                 sol.scans.append(Scan(current_signed_library, []))
-                if len(libraries_to_sign):
-                    current_signed_library = libraries_to_sign.pop()
+                if len(sol.libraries_to_sign):
+                    current_signed_library = sol.get_next_library()
                     remaining_time_to_sign = current_signed_library.sign_time
 
             remaining_time_to_sign -= 1
 
-            for library in signed_libraries:
+            for library in sol.signed_libraries:
                 nb_book_scaned = 0
-                for book in library.books:
+                while len(library.books):
+                    book = library.books.pop()
                     if nb_book_scaned == library.books_per_day:
                         break
 
@@ -43,5 +63,6 @@ class GreedySolution(Solution):
                         nb_book_scaned += 1
                         book_scanned[book.id] = True
                         sol.scan_book(library, book)
+                        sol.remove_book_in_librairies(book)
 
         return sol
--- hashcode/model/library.py
+++ hashcode/model/library.py
@@ -1,11 +1,14 @@
 from hashcode.model.book import Book
+# from heapq import heappush, heappop
 from typing import List
 
 
 class Library:
     def __init__(self, books: List[Book], sign_time: int, books_per_day: int, id: int):
         self.id = id
-        self.books = set(books)
+        self.books = []
+        for b in books:
+            self.books.heappush(b)
         self.sign_time = sign_time
         self.books_per_day = books_per_day
         self.compute_score()
@@ -13,11 +16,18 @@ class Library:
     def get_score(self):
         return self.score
 
-    def update(self, books_scanned: List[Book]):
-        for bc in books_scanned:
-            if bc in self.books:
-                self.books.remove(bc)
-        self.compute_score()
+    def remove_book(self, book_scanned: Book):
+        if book_scanned in self.books:
+            self.books.remove(book_scanned)
+            self.score -= self.score_per_book(book_scanned)
+
+    def score_per_book(self, book: Book):
+        return self.books_per_day * book.score / self.sign_time
 
     def compute_score(self):
-        self.score = self.books_per_day * sum([b.score for b in self.books]) / self.sign_time
+        self.score = sum([self.score_per_book(b) for b in self.books])
+
+    def update_score(self, books_scanned: List[Book]):
+        for sb in books_scanned:
+            if sb in self.book:
+                self.score -= self.score_per_book(sb)
