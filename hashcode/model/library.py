f804c88478eaad45d21e9d53431f0320c38712ac
flagos <flagospub@gmail.com>
2020-02-20 20:26:36 +0100

    use set for books

--- hashcode/model/library.py
+++ hashcode/model/library.py
@@ -5,6 +5,6 @@ from typing import List
 class Library:
     def __init__(self, books: List[Book], sign_time: int, books_per_day: int, id: int):
         self.id = id
-        self.books = books
+        self.books = set(books)
         self.sign_time = sign_time
         self.books_per_day = books_per_day
